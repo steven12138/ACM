0449955cae9cd1e405a8bf2210d5a922280013c1
steven12138 <18611209860@163.com>
2022-09-21 09:49:30 +0800

    init

---
+++ src/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#ifndef ONLINE_JUDGE
+#define DEBUG(X) std::cout<<X<<std::endl;
+#else
+#define DEBUG(X) ;
+#endif
+
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    DEBUG(a)
+    return 0;
+}
